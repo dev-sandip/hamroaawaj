a1af56692bfc98043cf036318ecfe76d874b68bf
AnuragDahal <freefirepros2020@gmail.com>
2024-01-20 01:17:03 +0545

    postcard

--- frontend/src/components/pages/landing/landing-page.tsx
+++ frontend/src/components/pages/landing/landing-page.tsx
@@ -2,25 +2,26 @@ import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import Profile from "../profile/profile";
 import Side from "@/components/shared/sidebar/Side";
+import Postcard from "../postcard/postcard";
 
 const LandingPage = () => {
   return (
     <>
-    <Side/>
-    <Profile/>
-    <div className="flex flex-col gap-4">
-      <Link to="/login">
-        <Button>Login</Button>
-      </Link>
-      <Link to="/report">
-        <Button>Report</Button>
-      </Link>
-      <Link to="/profile">
-        <Button>Profile</Button>
-      </Link>
-    </div>
-      </>
-  )
+      <Side />
+      <Postcard />
+      <div className="flex flex-col gap-4">
+        <Link to="/login">
+          <Button>Login</Button>
+        </Link>
+        <Link to="/report">
+          <Button>Report</Button>
+        </Link>
+        <Link to="/profile">
+          <Button>Profile</Button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default LandingPage;
