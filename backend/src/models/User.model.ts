808820e5ef6ac1a1e2cfaa8f382c015fba2235ae
Sandip Sapkota <sandipsapkota9844@gmail.com>
2024-01-20 00:27:35 +0545

    🐞 fix: fixed user model

--- backend/src/models/User.model.ts
+++ backend/src/models/User.model.ts
@@ -60,6 +60,10 @@ const UserSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
 
 });
 
