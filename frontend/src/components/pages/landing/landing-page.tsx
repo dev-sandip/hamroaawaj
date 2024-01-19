6aa93703e96c56b013c9cea3b6f33b1ed1ab29e6
Tilak Thapa <iamjrtilak@gmail.com>
2024-01-19 22:12:08 +0545

    Add ProfilePage component to routes

--- frontend/src/components/pages/landing/landing-page.tsx
+++ frontend/src/components/pages/landing/landing-page.tsx
@@ -1,6 +1,19 @@
+import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
 const LandingPage = () => {
-  return <Link to="/report">Report</Link>;
+  return (
+    <div className="flex flex-col gap-4">
+      <Link to="/login">
+        <Button>Login</Button>
+      </Link>
+      <Link to="/report">
+        <Button>Report</Button>
+      </Link>
+      <Link to="/profile">
+        <Button>Profile</Button>
+      </Link>
+    </div>
+  );
 };
 export default LandingPage;
