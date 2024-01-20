3aa8607766edfe0cf5013a20497e71bbe773f72d
Sandip Sapkota <sandipsapkota9844@gmail.com>
2024-01-20 17:07:28 +0545

    🐞 fix: typo

--- backend/src/router/auth.route.ts
+++ backend/src/router/auth.route.ts
@@ -2,11 +2,7 @@ import { Router } from "express";
 import UserAuthController from "../controllers/authController";
 import { verifyToken } from "../utils/token-manager";
 const authRouter = Router();
-//Create a new email user.
-authRouter.post(
-    "/register",
-    UserAuthController.Register
-);
+authRouter.post("/register", UserAuthController.Register);
 authRouter.post("/login", UserAuthController.Login);
 authRouter.get("/verify", verifyToken, UserAuthController.verifyUser);
 authRouter.post("/logout", UserAuthController.logout)
