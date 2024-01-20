fafdaf6a4462f811bde3ecb190c02d3cf90318cc
Tilak Thapa <iamjrtilak@gmail.com>
2024-01-20 20:27:05 +0545

    Update sidebar height in Side component

--- frontend/src/components/shared/sidebar/Side.tsx
+++ frontend/src/components/shared/sidebar/Side.tsx
@@ -80,7 +80,7 @@ export default function Side() {
       transition={{ duration: 0.5 }}
       className={cn(shadows.smm)}
     >
-      <aside className="flex-1 relative w-64 h-[calc(100vh-100px)] flex-col flex overflow-y-auto border-r bg-muted px-6 py-8">
+      <aside className="flex-1 relative w-64 h-[calc(100vh-80px)] flex-col flex overflow-y-auto border-r bg-muted px-6 py-8">
         <div className="mt-6 flex flex-1 flex-col justify-between flex-grow">
           <nav className="-mx-3 space-y-6  flex flex-col justify-between flex-grow">
             <div className="space-y-3 flex-grow">
