117f09272404ec96c8359035988e76aac03f0455
Tilak Thapa <iamjrtilak@gmail.com>
2024-01-20 08:04:40 +0545

    Add @radix-ui/react-scroll-area package

--- frontend/src/components/layouts/root-layout.tsx
+++ frontend/src/components/layouts/root-layout.tsx
@@ -1,14 +1,15 @@
 // import Footer from "../shared/footer/footer";
 import Navbar from "../shared/navbar/navbar";
 import Side from "../shared/sidebar/Side";
+import { ScrollArea } from "../ui/scroll-area";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex flex-col justify-between h-screen">
       <Navbar />
-      <div className="flex">
+      <div className="flex h- overflow-hidden">
         <Side />
-        {children}
+        <ScrollArea className="flex-1">{children}</ScrollArea>
       </div>
       {/* <Footer /> */}
     </main>
