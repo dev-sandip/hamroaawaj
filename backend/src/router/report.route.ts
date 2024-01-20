e9de14d0fc00ecacfdae0301282e4450838b3265
Sandip Sapkota <sandipsapkota9844@gmail.com>
2024-01-20 11:22:23 +0545

    🐞 fix: fixed route

--- backend/src/router/report.route.ts
+++ backend/src/router/report.route.ts
@@ -11,6 +11,6 @@ ReportRouter.put("/compeleteReport", ReportController.isCompleted);
 ReportRouter.post("/comment", ReportController.Comment);
 ReportRouter.get("/id/:id", ReportController.getReportById);
 ReportRouter.get("/fetchComments", ReportController.fetchComments);
-ReportRouter.get("/vote", ReportController.vote);
+ReportRouter.post("/vote", ReportController.vote);
 ReportRouter.get("/searchByFilter", ReportController.searchByFilter);
 export default ReportRouter;
