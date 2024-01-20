e893ca268d5a30253623de304ab4785041b7645b
Sandip Sapkota <sandipsapkota9844@gmail.com>
2024-01-20 07:56:30 +0545

    Revert "✨feature: fetchComments by report id"
    This reverts commit 5c6abb95eb682f4218e7bffd435b56745af9025f.

--- backend/src/router/report.route.ts
+++ backend/src/router/report.route.ts
@@ -5,11 +5,13 @@ const ReportRouter = Router();
 
 ReportRouter.post("/post", ReportController.ReportPost);
 ReportRouter.get("/fetchAll", ReportController.FetchReports);
-ReportRouter.get("/fetchwithoutlabel", ReportController.FetchReportWithoutLabel);
+ReportRouter.get(
+  "/fetchwithoutlabel",
+  ReportController.FetchReportWithoutLabel
+);
 ReportRouter.put("/updateLabel", ReportController.UpdateLabel);
 ReportRouter.put("/compeleteReport", ReportController.isCompleted);
 ReportRouter.post("/comment", ReportController.Comment);
 ReportRouter.get("/id/:id", ReportController.getReportById);
-ReportRouter.get("/fetchComments", ReportController.fetchComments);
 
 export default ReportRouter;
