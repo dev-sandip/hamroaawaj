b33cdb47dce87f9856e074b117a09f80da4c425a
Tilak Thapa <iamjrtilak@gmail.com>
2024-01-20 12:09:19 +0545

    Add @radix-ui/react-label package and update feedback form

--- frontend/src/components/pages/feedback/feedback.tsx
+++ frontend/src/components/pages/feedback/feedback.tsx
@@ -1,50 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import toast from "react-hot-toast";
+
 const Feedback = () => {
   return (
     <>
       <form className="max-w-md mx-auto mt-20 p-6 bg-white border rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold mb-6">Feedback htmlForm</h2>
+        <h2 className="text-2xl font-bold mb-6">Feedback Form</h2>
         <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
-            Name:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="name"
-            type="text"
-            placeholder="Enter your name"
-          />
+          <Label htmlFor="name">Name:</Label>
+          <Input id="name" type="text" placeholder="Enter your name" />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
-            Email:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Enter your email"
-          />
+          <Label htmlFor="email">Email:</Label>
+          <Input id="email" type="email" placeholder="Enter your email" />
         </div>
         <div className="mb-4">
-          <label
-            className="block text-gray-700 font-bold mb-2"
-            htmlFor="feedback"
-          >
-            Feedback:
-          </label>
-          <textarea
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          <Label htmlFor="feedback">Feedback:</Label>
+          <Textarea
             id="feedback"
             rows={5}
             placeholder="Enter your feedback"
-          ></textarea>
+          ></Textarea>
         </div>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            toast.success("Thank you for your feedback!");
+          }}
+          className="w-full"
           type="submit"
         >
           Submit
-        </button>
+        </Button>
       </form>
     </>
   );
