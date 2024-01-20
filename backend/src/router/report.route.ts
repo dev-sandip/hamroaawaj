5c6abb95eb682f4218e7bffd435b56745af9025f
Sandip Sapkota <sandipsapkota9844@gmail.com>
2024-01-20 07:51:53 +0545

    ✨feature: fetchComments by report id

--- backend/src/router/report.route.ts
+++ backend/src/router/report.route.ts
@@ -5,13 +5,11 @@ const ReportRouter = Router();
 
 ReportRouter.post("/post", ReportController.ReportPost);
 ReportRouter.get("/fetchAll", ReportController.FetchReports);
-ReportRouter.get(
-  "/fetchwithoutlabel",
-  ReportController.FetchReportWithoutLabel
-);
+ReportRouter.get("/fetchwithoutlabel", ReportController.FetchReportWithoutLabel);
 ReportRouter.put("/updateLabel", ReportController.UpdateLabel);
 ReportRouter.put("/compeleteReport", ReportController.isCompleted);
 ReportRouter.post("/comment", ReportController.Comment);
 ReportRouter.get("/id/:id", ReportController.getReportById);
+ReportRouter.get("/fetchComments", ReportController.fetchComments);
 
 export default ReportRouter;
